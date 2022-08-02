286870713b3aec1c4e7c50d7440af00d974b172e
AkifAhmedTapmad <Akif.Ahmed@tapmad.com>
2022-08-02 15:42:51 +0500

    Initial Commit

---
+++ SimpleTableSwiftUI/SimpleTableSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTableSwiftUIApp.swift
+//  SimpleTableSwiftUI
+//
+//  Created by Akif Ahmed on 02/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTableSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
